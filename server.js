0f41f258c60b989a64052833c74072cf433d7964
Ian Beihl <ianbeihl@gmail.com>
2018-06-27 11:38:28 -0700

    moved endpoints to express router

--- server.js
+++ server.js
@@ -1,72 +1,23 @@
 'use strict';
 
 const express = require('express');
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
+//const data = require('./db/notes');
+// const simDB = require('./db/simDB');
+// const notes = simDB.initialize(data);
 const { PORT } = require('./config');
 //const { logger } = require('./middleware/logger');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router/notes.router');
 
 //middleware
 app.use(morgan('dev'));
-//app.use(logger);
 
 app.use(express.static('public'));
 
 app.use(express.json());
 
-//endpoints
-app.put('/api/notes/:id', (req, res, next) => {
-    const id = req.params.id;
-
-    const updateObj = {};
-    const updateFields = ['title', 'content'];
-
-    updateFields.forEach(field => {
-        if(field in req.body){
-            updateObj[field] = req.body[field];
-        }
-    });
-
-    notes.update(id, updateObj, (err, item) => {
-        if(err){
-            return next(err);
-        }
-        if(item) {
-            res.json(item);
-        }
-        else{
-            next();
-        }
-    });
-});
-
-
-app.get('/api/notes', (req, res, next) => {
-    const {searchTerm} = req.query;
-
-    notes.filter(searchTerm, (err, list) => {
-        if(err){
-            return next(err);
-        }
-        res.json(list);
-    });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-    const id = req.params.id;
-
-    notes.find(id, (err, item) => {
-        if(err){
-            return next(err);
-        }
-        res.json(item);
-    });
-});
-
-
+app.use('/api/notes', router);
 
 //error handling
 app.use(function(req, res, next){
