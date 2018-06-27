af5d2b2371ce820e6df7a60e7bb64808a232468f
Ian Beihl <ianbeihl@gmail.com>
2018-06-27 11:50:44 -0700

    added new note post endpoint

--- router/notes.router.js
+++ router/notes.router.js
@@ -29,6 +29,28 @@ router.put('/:id', (req, res, next) => {
     });
 });
 
+router.post('/', (req, res, next) => {
+    const { title, content } = req.body;
+
+    const newItem = { title, content};
+        if(!newItem.title){
+            const err = new Error('missing title in the request body');
+            err.status = 404;
+            return next(err);
+        }
+        notes.create(newItem, (err, item) => {
+            if (err){
+                return next(err);
+            }
+            if (item) {
+                res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+            }
+            else{
+                next();
+            }
+        });
+    });
+
 
 router.get('/', (req, res, next) => {
     const {searchTerm} = req.query;
